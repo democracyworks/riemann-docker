1c3dcb53b7768c9cffbc124a2e91424ccb022ae7
Nathan Herzing <nherzing@gmail.com>
2014-05-02 00:01:37 -0400

    [NH] Make librato email address customizable.

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ -z $LIBRATO_KEY ]]; then
-  echo "Set LIBRATO_KEY as your librato api key"
+if [[ -z $LIBRATO_KEY || -z $LIBRATO_EMAIL ]]; then
+  echo "Set LIBRATO_EMAIL as your librato email address and LIBRATO_KEY as your librato api key"
   exit 1
 fi
 
@@ -12,7 +12,7 @@ if [[ -n $HIPCHAT_TOKEN && -n $HIPCHAT_ROOM ]]; then
   mv temp /etc/riemann/riemann.config
 fi
 
-echo "(def librato-key \"$LIBRATO_KEY\")" | cat - /etc/riemann/riemann.config > temp
+echo "(def librato-email \"$LIBRATO_EMAIL\")(def librato-key \"$LIBRATO_KEY\")" | cat - /etc/riemann/riemann.config > temp
 mv temp /etc/riemann/riemann.config
 
 /usr/bin/riemann /etc/riemann/riemann.config
